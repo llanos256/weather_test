a241992910e8300b6c69280a4c3a9217ae538d64
llanos256 <davidllanos78@gmail.com>
2025-05-02 11:40:05 -0500

    New models

--- src/main/java/com/weathertest/weather/service/WeatherService.java
+++ src/main/java/com/weathertest/weather/service/WeatherService.java
@@ -1,8 +1,19 @@
 package com.weathertest.weather.service;
 
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.weathertest.weather.models.WeatherBody;
+//import com.weathertest.weather.repositories.RequestLogsRepository;
 
 @Service
 public class WeatherService {
-
+     
+	//@Autowired
+	//private RequestLogsRepository logsRepo;
+	
+	public WeatherBody getWeather(String city) {
+		
+		return null;
+	}
 }
